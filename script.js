8937050b659cd3827937bf88f83d5d6cacef92d3
afalmendrala <afalmendrala16@gmail.com>
2022-05-01 19:09:22 +0800

    fix: Change playRound variables and return values

--- script.js
+++ script.js
@@ -23,10 +23,115 @@ function tie(player){
     return `Tie! both players chose ${player}!`;
 }
 function playRound(player, computer){
-    
-}
-const playerSelection = playerPlay();
-const computerSelection = computerPlay().toLowerCase();
+    let playerScore = 0;
+    let computerScore = 0;
 
-playRound(playerSelection, computerSelection);
+    let tieGame = function(){
+        return tie(player);
+    }
+    let winGame = function(){
+        return playerWin(player, computer);
+    }
+    let loseGame = function(){
+        return playerLose(player, computer);
+    }
+    switch(player){
+        case 'rock':
+            if(computer == 'rock'){
+                return {
+                    msg:tieGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }
+            else if (computer == 'paper'){
+                computerScore += 1;
+                return {
+                    msg:loseGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }
+            else if (computer == 'scissors'){
+                playerScore += 1;
+                return {
+                    msg:winGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }
+        case 'paper':
+            if(computer == 'rock'){
+                playerScore += 1;
+                return {
+                    msg:winGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }
+            else if (computer == 'paper'){
+                return {
+                    msg:tieGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }
+            else if (computer == 'scissors'){
+                computerScore += 1;
+                return {
+                    msg:loseGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }
+        case 'scissors':
+            if(computer == 'rock'){
+                playerScore += 1;
+                return {
+                    msg:winGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }
+            else if (computer == 'paper'){
+                computerScore += 1;
+                return {
+                    msg:loseGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }
+            else if (computer == 'scissors'){
+                return {
+                    msg:tieGame(),
+                    playerScore:playerScore,
+                    computerScore:computerScore
+                };
+                break;
+            }        
+    }
+}
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+for(let i = 0; i < 5; i++){
+    playerSelection = playerPlay();
+    computerSelection = computerPlay().toLowerCase();
+    let results = playRound(playerSelection, computerSelection);
+    let msg = results.msg;
+    playerScore += results.playerScore;
+    computerScore += results.computerScore;
+    console.log(msg + ` Player score: ${playerScore}, Computer Score: ${computerScore}`);
+}
+
 // console.log(computerSelection);
